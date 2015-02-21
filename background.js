2e60009cbbabe1ad4cb25003501b6cd0a894b898
Scott Pigeon <pigeontech@gmail.com>
2015-02-21 13:40:21 -0500

    Path and Selection added

--- background.js
+++ background.js
@@ -3,6 +3,7 @@ console.log("Extension loaded. Waiting on Caret...");
 chrome.runtime.onMessageExternal.addListener(function(message, sender, sendResponse) {
     if (message.data === "sayHello") {
       console.log("Hello World!");
-      alert("Hello World!");
+      console.log("Path:", message.context.path);
+      console.log("Selected Text:\n", message.context.selection);
     }
 });
